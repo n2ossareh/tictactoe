c5d9bacb550db5b73ef64c78bfe48a87b69628f7
Na Zo <98631994+n2ossareh@users.noreply.github.com>
2025-05-05 01:51:27 +0000

    complete changes to tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,11 +3,18 @@ Tic Tac Toe Player
 """
 
 import math
+# import pygame
+import sys
+# import numpy as np
+import copy
+import os
+
 
 X = "X"
 O = "O"
 EMPTY = None
 
+optimalAction = None
 
 def initial_state():
     """
@@ -22,21 +29,22 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    if cellsCount(EMPTY, board) == 9:
-        return X
+    if isBoard3x3(board) == False:
+        return None
 
-    # if terminal(board):
-    #     return None 
+    if terminal(board) == True:
+        return None
 
-    countX = cellsCount(X, board)
-    countO = cellsCount(O, board)
+    occupiedCells = 9 - cellsCount(EMPTY, board)
 
-    takenCells = countX + countO
-    if takenCells < 9 and takenCells % 2 == 0:
-        return X
+    if occupiedCells < 9:
+        if occupiedCells % 2 == 0:
+            return X
+        else:
+            return O
     else:
-        return O
-    
+        return None
+
 
 
 def actions(board):
@@ -44,14 +52,16 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
     actions = set()
+    
+    if isBoard3x3(board) == False:
+        return actions
+
     for i in range(3):
         for j in range(3):
-            if board[i][j] != EMPTY:
+            if board[i][j] is not None:
                 continue
             else:
                 actions.add((i, j))
-    
-    
     return actions
 
 
@@ -59,50 +69,97 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    countX = cellsCount(X, board)
-    countO = cellsCount(O, board)
-    row = action[0]
-    col = action[1]
-    if (countX == countO):
-        board[row][col] = X
-    else:
-        board[row][col] = O
+    if terminal (board):
+        return utility(board)
 
-    return board
+    new_board = copy.deepcopy(board)
+
+    if action is not None:
+      row = action[0]
+      col = action[1]
+    else:
+      return None
+
+    if row != None and col != None:
+      if row not in range(3) or col not in range(3):
+          raise Exception("out-of-bound index.")
+      if new_board[row][col] in {X, O}:
+          raise Exception("Cell is occupied")
+
+      new_board[row][col] = player(board)
+
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if terminal(board) == True:
-        return utility(board)
+    if isBoard3x3(board) == False:
+        return None
+
+    if isInitialState(board) == True:
+        return None
+
+    # Hotizontal
+    for i in range(3):
+        if (board[i][0] is not None and
+            board[i][0] == board[i][1] and
+            board[i][1] == board[i][2]):
+            return board[i][0]
+
+    # Vertical
+    for i in range(3):
+        if (board[0][i] is not None and
+            board[0][i] == board[1][i] and
+            board[1][i] == board[2][i]):
+            return board[0][i]
+
+    # Diagnal
+    if ((board[1][1] is not None and
+        board[0][0] == board[1][1] and
+        board[1][1] == board[2][2]) or
+        (board[0][2] == board[1][1] and
+         board[1][1] == board[2][0])):
+        return board[1][1]
+
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    if isinstance(board, int):
+        return False
+
     # winning combinations
     for row in range (3):
-        if board[row][0] != EMPTY and board[row][0] == board[row][1] and board[row][1] == board[row][2]:
-            return True
-    
-    for col in range (3):
-        if board[0][col] != EMPTY and board[0][col] == board[1][col] and board[1][col] == board[2][col]:
+        if (isCellOccupied(board, row, 0) and
+            board[row][0] == board[row][1] and
+            board[row][1] == board[row][2]):
             return True
 
-    if board[0][0] != EMPTY and board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+    for col in range (3):
+        if (isCellOccupied(board, 0, col) and
+            board[0][col] == board[1][col] and
+            board[1][col] == board[2][col]):
+            return True
+
+    if ((isCellOccupied(board, 1, 1) and
+        ((board[0][0] == board[1][1] and
+        board[1][1] == board[2][2]) or
+        (board[0][2] is not None and
+         board[0][2] == board[1][1] and
+         board[1][1] == board[2][0])))):
         return True
 
-    if board[0][2] != EMPTY and board[0][2] == board[1][1] and board[1][1] == board[2][0]:
-        return True 
 
     # possibly drawn
     if cellsCount(EMPTY, board) == 0:
         return True
-    
-    return False    
+
+    return False
 
 
 
@@ -111,33 +168,122 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    if isInitialState(board) == True:
-        return 0
-    
+    if board is None:
+      sys.exit()
 
-    countX = cellsCount(X, board)
+    if isBoard3x3(board) == False:
+      if len(board) > 0:
+          str_value = board[0]
+          if str_value.isdigit():
+              return int(str_value)
 
-    countO = cellsCount(O, board)
-
-    if countX + countO %2 == 0:
-        return X
+    if terminal(board):
+        _winner = winner(board)
+        if _winner == X:
+            return 1
+        elif _winner == O:
+            return -1
+        else:
+            return 0
     else:
-        return O
-
+        return None
 
 def minimax(board):
-    """
-    Returns the optimal action for the current player on the board.
-    """
-    if terminal(board):
-        return None
-    
-    moves = actions(board)
-    return moves[0]
+  if terminal(board):
+      return None
+  
+  _player = player(board)
+  if _player is None or terminal(board) == True:
+        return utility(board)
 
-# returns the count of cells containg piece (X, O or EMPTY) on the given board    
+  if _player == X:
+    score = Max_Val(board)[0]
+    return score
+
+  if _player == O:
+    score = Min_Val(board)[0]
+    return score
+
+def best_move(main_board):
+    if terminal(main_board) == True:
+      return utility(main_board)
+
+    board = copy.deepcopy(main_board)
+
+    _player = player(board)
+    if _player == X:
+      best_score = float("-inf")
+    elif _player == O:
+      best_score = float("inf")
+    else:
+      best_score = 0
+
+    best_move = None
+    best_move = None
+    available_moves = actions(board)
+    print(f"line 335 - available_moves = {available_moves}")
+    for move in available_moves:
+      board = result(board, move)
+
+      score = minimax(board)
+      _player = player(board)
+      if _player == X:
+        if score > best_score:
+          best_score = score
+          best_move = move
+        else:
+          if score < best_score:
+            best_score = score
+            best_move = move
+
+    if best_move != None:
+      main_board[best_move[0]][best_move[1]] = _player
+
+def Max_Val(board):
+    if terminal(board) == True:
+        return None, utility(board)
+
+    maxEval = float("-inf")
+    best_move = None
+
+
+    for action in actions(board):
+      _, eval = Min_Val(result(board, action))
+      if eval > maxEval:
+        maxEval = eval
+        best_move = action
+    return best_move, maxEval
+
+def Min_Val(board):
+    if terminal(board) == True:
+        return None, utility(board)
+
+    minEval = float("inf")
+    best_move = None
+
+    for action in actions(board):
+      _, eval = Max_Val(result(board, action))
+      if eval < minEval:
+        minEval = eval
+        best_move = action
+
+    return best_move, minEval
+
+
+def isCellOccupied(board, row, col):
+    if board is not None:
+        if row in range(3) and col in range(3):
+            if board[row][col] is None:
+                return False
+            else:
+                return True
+
+# returns the count of cells containg piece (X, O or EMPTY) on the given board
 def cellsCount(piece, board):
     count = 0
+    if board is None or isBoard3x3(board) == False:
+        return count
+
     for i in range(3):
         for j in range(3):
             if board[i][j] == piece:
@@ -148,5 +294,13 @@ def isInitialState(board):
     if cellsCount(EMPTY, board) == 9:
         return True
 
-    return False 
-        
+    return False
+
+def isBoard3x3(board):
+    # Check if the board is a list
+    if isinstance(board, list):
+        # Check if it contains exactly 3 sublists
+        if len(board) == 3 and all(isinstance(sublist, list) and len(sublist) == 3 for sublist in list(board)):
+            return True
+    return False
+
